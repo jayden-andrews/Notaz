6f53234071972720af11405933e8122bf59113d0
Jayden Andrews <jaydenrandrews@gmail.com>
2026-04-30 16:04:28 -0400

    Implemented the repositories

--- backend/src/main/java/com/example/backend/repositories/ExerciseRepository.java
+++ backend/src/main/java/com/example/backend/repositories/ExerciseRepository.java
@@ -1,4 +1,13 @@
 package com.example.backend.repositories;
 
-public interface ExerciseRepository {
+import java.util.List;
+import com.example.backend.entities.Clef;
+import com.example.backend.entities.Exercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+    List<Exercise> findByClef(Clef clef);
 }
--- backend/src/main/java/com/example/backend/repositories/NoteRepository.java
+++ backend/src/main/java/com/example/backend/repositories/NoteRepository.java
@@ -1,4 +1,10 @@
 package com.example.backend.repositories;
 
-public interface NoteRepository {
+import com.example.backend.entities.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    Note findByStaffPosition(int staffPosition);
 }
